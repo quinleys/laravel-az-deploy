2b5639ce2617090a9edd3cf1326199fbdf494f74
Quinten Leysen <quinleys@student.arteveldehs.be>
2020-03-22 02:55:54 +0100

    update

--- resources/views/hometext/edit.blade.php
+++ resources/views/hometext/edit.blade.php
@@ -6,7 +6,7 @@
 <div class="container-fluid">
     <div class="row mb-2">
         <div class="col-sm-6">
-        <h1>Edit {{$post['title']}}</h1>
+        <h1>Edit {{$post['id']}}</h1>
         </div>
         <div class="col-sm-6">
            <ol class="breadcrumb float-sm-right">
@@ -17,7 +17,7 @@
            <a href="/hometext">Home Audio</a>
            </li>
            <li class="breadcrumb-item active">
-           Edit {{$post['title']}}
+           Edit {{$post['id']}}
            </li>
            </ol>
         </div>
@@ -30,9 +30,10 @@
 <form action="{{ route('hometext.update',$post['id']) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
+  <p>{{$post['audio']}}</p>
   <div class="form-group">
-    <label for="title">Title</label>
-    <input type="text" name="title" class="form-control" value="{{$post['title']}}">
+    <label for="exampleFormControlFile1">Audio input</label>
+    <input type="file" name="audio" class="form-control-file" id="exampleFormControlFile1">
   </div>
     <button type="submit" class="btn btn-primary">Submit</button>
     <a href="/hometext" class="btn btn-danger">Cancel </a>
